3b5632b9c4cacd00586c998a4f67e37bb4015a85
luknp <luk.npl@gmail.com>
2021-08-18 14:17:17 +0200

    refac: storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,16 +1,15 @@
-const storageThemeModeKey = 'themeKey';
+const storageThemeModeKey = 'darkMode';
 
 const saveThemeMode = (isDark: boolean) => {
   localStorage.setItem(storageThemeModeKey, String(isDark));
 };
 
-const loadThemeMode = () => {
+const loadThemeMode = (): boolean => {
   const darkMode = localStorage.getItem(storageThemeModeKey);
   if (darkMode) {
     return JSON.parse(darkMode);
   }
-
-  return null;
+  return true;
 };
 
 const storage = {
